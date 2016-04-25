a0787aa230209347d16e0d8c75c6d6119fadcdbe
napo <napo@fbk.eu>
2016-04-25 12:23:57 +0200

    file finale

--- estrai.sh
+++ estrai.sh
@@ -2,4 +2,5 @@ if [ -f full-pdf.pdf ];
 then 
 	wget "http://www1.agenziaentrate.it/elenchi%205x1000%202014/Elenco%20completo%20dei%20beneficiari.pdf" -O full-pdf.pdf
 fi
-tabula --spreadsheet -a 54.0,51.0,537.097,782.487 -p all -o completo.csv full-pdf.pdf
+tabula --spreadsheet -a 71.044,40.521,537.301,789.901 -p all -o elencobeneficiari.csv full-pdf.pdf
+
